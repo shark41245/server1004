871e878ef5d9574721bda59ce56c6d731a044999
shark41245 <machines0101@naver.com>
2026-04-09 19:10:03 +0900

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import SignupPage from "./pages/SignupPage";
 import AdminPage from "./pages/AdminPage";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/admin1004" element={<AdminPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/admin1004" element={<AdminPage />} />
+    </Routes>
   );
 }
