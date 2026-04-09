bb4ff75d6837c75d4ebb1002edf00c6cf4f607aa
shark41245 <machines0101@naver.com>
2026-04-09 12:17:42 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,14 @@
-import { Link, Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import SignupPage from "./pages/SignupPage";
 import AdminPage from "./pages/AdminPage";
+import SignupPage from "./pages/SignupPage";
 
-function AppShell() {
-  const location = useLocation();
-  const isHome = location.pathname === "/";
-
+export default function App() {
   return (
-    <div className={isHome ? "app-shell ocean-theme" : "app-shell inner-theme"}>
-      <header className={isHome ? "floating-header" : "site-header"}>
-        <div className="header-inner">
-          <Link to="/" className="site-logo">
-            Blue Horizon
-          </Link>
-
-          <nav className="site-nav">
-            <Link to="/">홈</Link>
-            <Link to="/signup">회원가입</Link>
-            <Link to="/admin">관리자</Link>
-          </nav>
-        </div>
-      </header>
-
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/admin" element={<AdminPage />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/admin" element={<AdminPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+    </Routes>
   );
 }
-
-export default AppShell;
